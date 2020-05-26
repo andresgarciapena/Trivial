5d2ad0de089c70f9b089c45b9d8d24b80c6a7a6b
Andres Garcia Peña <andres.garcia.p.91@gmail.com>
2020-05-26 17:27:05 +0200

    Constantes

---
+++ Trivial/Trivial/Common/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Trivial
+//
+//  Created by Andres Garcia Peña on 26/05/2020.
+//  Copyright © 2020 Andres Garcia. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+
+    static let apiUrl = "https://opentdb.com/api.php?amount=%@&category=%@"
+}
---
+++ Trivial/Trivial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivial
+//
+//  Created by Andres Garcia Peña on 26/05/2020.
+//  Copyright © 2020 Andres Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
