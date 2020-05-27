6695fd300b965d9c6662e5c895eda30ee52aa9fe
Andres Garcia Peña <andres.garcia.p.91@gmail.com>
2020-05-27 18:17:35 +0200

    Picker para seleccionar la categoria

--- Trivial/Trivial/Features/CustomizeGame/View/CustomizeGameViewController.swift
+++ Trivial/Trivial/Features/CustomizeGame/View/CustomizeGameViewController.swift
@@ -8,21 +8,29 @@
 
 import UIKit
 
+
+
 class CustomizeGameViewController: UIViewController {
     
     @IBOutlet weak var playerNameTextField: UITextField!
     @IBOutlet weak var numberQuestionsTextField: UITextField!
     @IBOutlet weak var categoryTextField: UITextField!
     @IBOutlet weak var startGameButton: UIButton!
+    @IBOutlet weak var pickerContainer: UIView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var okPickerButton: UIButton!
     
     var presenter: CustomizeGamePresenter = CustomizeGamePresenter()
+    
+    //var pickerData: [PickerData] = [PickerData(name: "Dato1", number: 1), PickerData(name: "Dato2", number: 2)]
+    var categorySelected: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         presenter.viewRef = self
         presenter.viewDidLoad()
-        configureTextField()
+        configureView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,19 +39,75 @@ class CustomizeGameViewController: UIViewController {
         presenter.viewWillAppear(animated: animated, navigationController: navigationController)
     }
     
+    @objc func showPicker() {
+        pickerContainer.isHidden = false
+    }
+    
     func saveConfigurationGame() {
         guard let name = playerNameTextField.text, let nQuestions = numberQuestionsTextField.text, let category = categoryTextField.text else { return }
         
-        presenter.saveData(name: name, nQuestions: nQuestions, category: category)
+        if name.isEmpty || nQuestions.isEmpty || category.isEmpty {
+            
+        } else {
+            presenter.saveData(name: name, nQuestions: nQuestions, category: category)
+        }
     }
     
-    func configureTextField() {
+    func configureView() {
+        categoryTextField.delegate = self
+        categoryTextField.tintColor = UIColor.clear
+        
+        pickerContainer.isHidden = true
+        
         playerNameTextField.keyboardType = .asciiCapable
         numberQuestionsTextField.keyboardType = .numberPad
     }
 
     @IBAction func startGameButtonAction(_ sender: Any) {
         
-        saveConfigurationGame()
+        if categorySelected != 0 {
+            saveConfigurationGame()
+        } else {
+            
+        }
+    }
+    
+    @IBAction func okPickerButtonAction(_ sender: Any) {
+        
+        pickerContainer.isHidden = true
+        categoryTextField.resignFirstResponder()
+    }
+}
+
+extension CustomizeGameViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        showPicker()
+    }
+}
+
+extension CustomizeGameViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return Constants.pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        categoryTextField.text = Constants.pickerData[row].name
+        return Constants.pickerData[row].name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        categorySelected = Constants.pickerData[row].number
     }
 }
