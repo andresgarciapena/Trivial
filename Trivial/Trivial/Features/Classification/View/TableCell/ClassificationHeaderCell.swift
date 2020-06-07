074250be4cc541352d808ca22ddb5d39b1f8f793
Andres Garcia Peña <andres.garcia.p.91@gmail.com>
2020-06-07 20:56:24 +0200

    Vista de cada tipo de celda que mostrara la tabla

---
+++ Trivial/Trivial/Features/Classification/View/TableCell/ClassificationHeaderCell.swift
@@ -0,0 +1,28 @@
+//
+//  ClassificationHeaderCell.swift
+//  Trivial
+//
+//  Created by Andres Garcia Peña on 07/06/2020.
+//  Copyright © 2020 Andres Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ClassificationHeaderCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var correctQuestions: UILabel!
+    @IBOutlet weak var totalQuestions: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Trivial/Trivial/Features/Classification/View/TableCell/PlayerScoreCell.swift
@@ -0,0 +1,28 @@
+//
+//  PlayerScoreCell.swift
+//  Trivial
+//
+//  Created by Andres Garcia Peña on 07/06/2020.
+//  Copyright © 2020 Andres Garcia. All rights reserved.
+//
+
+import UIKit
+
+class PlayerScoreCell: UITableViewCell {
+
+    @IBOutlet weak var playerNameLabel: UILabel!
+    @IBOutlet weak var correctAnswersLabel: UILabel!
+    @IBOutlet weak var totalQuestionsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
